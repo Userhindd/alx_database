384b71c8009c9af911d6c1fa4724b2abc0c653b1
Userhindd <Oussellamhind@gmail.com>
2024-01-24 17:38:39 +0100

    w

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,2 +1,8 @@
--- show all databases
+-- This script lists all databases in the MySQL server
+
+-- Connect to the MySQL server with the provided credentials
+-- -h: Host (localhost in this case)
+-- -u: User (root in this case)
+-- -p: Prompt for password
+-- Query to list all databases
 SHOW DATABASES;
